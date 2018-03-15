283aff4c2bc054802a20c1f96b6a1d07633ae15a
NadiaMorales <nadia.morales.leiva@gmail.com>
2018-03-15 17:10:21 -0300

    agregando app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Home from './component/Home';
 import './App.css';
-import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Home />
-      </div>
+          <div className="App">
+            <header className="App-header">
+            <Row>
+              
+              <Col xsOffset={10} xs={1}>
+              <button type="button">☺</button>
+              </Col>
+              <Col  xs={1}>
+              <button type="button" >♥</button>
+              </Col>
+              
+            </Row>
+              <h1 className="App-title">Pokémon TCG</h1>
+            </header>
+            <input className="search" type="text" placeholder="Encuentra tu carta Pokémon"/>
+            <input className="btn" type="submit" value="Buscar"/>
+            <h1 className="result">Aqui puedes buscar tus cartas favoritas de Pokémon TCG</h1>
+            <Searchpoke /> 
+          </div>
     );
   }
 }
