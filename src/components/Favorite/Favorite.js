cf8e67b544070d419b6cd37cf83aab2616dd2c49
MrsPepa <jose.leon1996@gmail.com>
2018-03-16 03:36:26 -0300

    agregando vista favoritos

---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import FaStar from 'react-icons/lib/fa/star';
+import FaArrowLeft from 'react-icons/lib/fa/arrow-left';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import FavoritePokemon from './FavoritePokemon';
+import './../style.css';
+
+class Favorite extends Component {
+  render() {
+    return(
+      <div className="favorite">
+        <Row>
+          <Col xs={12} md={10}>
+            <h3>Añade a tus pokemon favoritos</h3>
+          </Col>
+          <Col xs={12} md={2}>
+            <FaStar className="star"/>
+          </Col>
+        </Row>
+        <FavoritePokemon />
+      </div>
+    )
+  }
+}
+
+export default Favorite;
--- src/components/Favorite/FavoritePokemon.js
+++ src/components/Favorite/FavoritePokemon.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import FaStar from 'react-icons/lib/fa/star';
-import FaArrowLeft from 'react-icons/lib/fa/arrow-left';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {List, ListItem} from 'material-ui/List';
 import './../style.css';
 
 class FavoritePokemon extends Component {
   render() {
     return(
-      
-        <Row className="barFavorite">
-          <Col xs={12} md={4}>
-            <FaArrowLeft />
-          </Col>
-          <Col xs={12} md={4}>
-            <h3>Aquí puedes ver tus Pokemon favoritos.</h3>
-          </Col>
-          <Col xs={12} md={4}>
-          <FaStar />
-          </Col>
-        </Row>
-      
+      <List className="pokemonFav">
+      <ListItem className="items" primaryText="Jigglypuff"/>
+      </List>
     )
   }
 }
